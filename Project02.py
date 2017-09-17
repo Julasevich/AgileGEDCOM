a78c2afb67ee8315cd7e6343e35e57e4b00b1e2d
Jacob Ulasevich <ulasevich.jacob@gmail.com>
2017-09-17 13:58:37 -0400

    Initial
    Initial commit, the following were Rowland's comments on the code:
    
    You handled most of the valid cases correctly except INDI and FAM which are exceptions.  INDI and FAM expect
    0 <id> INDI
    0 <id> FAM
    where the tag is the third token rather than the second token.
    
    You also didn’t recognize some invalid combinations properly.   
    
    GEDCOM tags are valid only at specific levels, e.g. 
    -	FAM, HEAD, INDI, NOTE, and TRLR are valid only at level 0
    -	BIRT, CHIL, DEAT, DIV, FAMC, FAMS, HUSB, MARR, NAME, SEX, WIFE are valid only at level 1
    -	DATE is valid only at level 2
    
    Are you aware of the string.join(sequence) method? E.g.
    	“|”.join(‘apples’, ‘oranges’, ‘pears’) == “apples|oranges|pears”
    You can create a string from a sequence, e.g. list, set, dict, separated by the specified string.

---
+++ Project02.py
@@ -0,0 +1,68 @@
+#Jacob Ulasevich
+#9/9/2017
+#Project 02
+
+#Get file
+gedFile  = open("MyFamily.ged", "r")
+#Acceptable tags
+projectTags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
+
+#Main loop
+for line in gedFile:
+    #Common Variables
+    #Split words of line up, may come with spaces so must strip later
+    words = line.split()
+    level = words[0]
+    #Tag Line
+    if level == "0":
+        if len(words) >= 3:
+            tag = words[2]
+            lineID = words[1]
+            valid = ""
+            if tag in projectTags:
+                valid = "Y"
+            else:
+                valid = "N"
+            #Input
+            print("--> " + line.strip())
+            #Output
+            print("<-- " + level.strip() + "|" + lineID.strip() + "|" + tag + "|" + valid.strip())
+            #Space out pairings of i/o
+            print("")
+        else:
+            tag = words[1]
+            valid = ""
+            if tag in projectTags:
+                valid = "Y"
+            else:
+                valid = "N"
+            #Input
+            print("--> " + line.strip())
+            #Output
+            print("<-- " + level.strip() + "|" + tag + "|" + valid.strip())
+            #Space out pairings of i/o
+            print("")
+    else:
+        tag = words[1]
+        valid = ""
+        arguments = ""
+        if len(words) >= 2:
+            for word in words[2:]:
+                arguments = arguments + " " + word
+        else:
+            arguments = ""
+        if tag in projectTags:
+            valid = "Y"
+        else:
+            valid = "N"
+        #Input
+        print("--> " + line.strip())
+        #Output
+        print("<-- " + level.strip() + "|" + tag.strip() + "|" + valid + "|" + arguments.strip())
+        #Space out pairings of i/o
+        print("")
+
+#Close file
+gedFile.close()
+
+    
